94253a679cd72fee986e95c425c6c7d444281f1b
Ivan L <ivan.lukyanchikov@gmail.com>
2020-05-10 03:58:53 +0300

    config

--- configs/dann_config.py
+++ configs/dann_config.py
@@ -6,7 +6,7 @@ IS_UNSUPERVISED = True
 GRADIENT_REVERSAL_LAYER_ALPHA = 1.0
 FREZE_BACKBONE_FEATURES = True
 
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 
 NUM_WORKERS = 4
 N_EPOCHS = 200
@@ -17,11 +17,11 @@ SAVE_MODEL_FREQ = 199
 ################### Model dependent parameters #########################
 
 CLASSES_CNT = 31
-MODEL_BACKBONE = "alexnet" # alexnet resnet50 vanilla_dann
+MODEL_BACKBONE = "resnet50" # alexnet resnet50 vanilla_dann
 DOMAIN_HEAD = "vanilla_dann"
 BACKBONE_PRETRAINED = True
-NEED_ADAPTATION_BLOCK = True # ="True" only for alexnet, ="False" for other types
-BLOCKS_WITH_SMALLER_LR = 2 # ="2" only for alexnet, ="0" for other types
+NEED_ADAPTATION_BLOCK = False # ="True" only for alexnet, ="False" for other types
+BLOCKS_WITH_SMALLER_LR = 0 # ="2" only for alexnet, ="0" for other types
 IMAGE_SIZE = 224
 DATASET = "office-31"
 SOURCE_DOMAIN = "amazon"
